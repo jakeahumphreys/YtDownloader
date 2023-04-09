e24ff9b771a79b514a146abcd6011f66801b2e47
jakeahumphreys <jakehumphreys@outlook.com>
2023-04-09 18:54:50 +0100

    Working on some logging for user feedback

---
+++ YtDownloader/Types/LogItem.cs
@@ -0,0 +1,8 @@
+﻿namespace YtDownloader.Types;
+
+public sealed class LogItem
+{
+    public DateTime OccurredAt { get; set; }
+    public string Icon { get; set; }
+    public string Message { get; set; }
+}
